c2e78bc54a61ffcec67c50c050ca484204e42914
Giyu2 <derdene719@gmail.com>
2025-02-21 02:02:39 -0800

    jgdis

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import axios from "axios";
+import { useEffect } from "react";
+export default function Home() {
+  const getDta = async () => {
+    const response = await axios.get(`http://localhost:4000`)
+    console.log(response);
+    
+  }
+
+  useEffect(()=> {
+    getDta()
+  },[ ])
+  return (
+    <div className="" >
+      Hello
+    </div>
+  );
+}
